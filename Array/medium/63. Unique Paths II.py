6b42fc4da9788ff02cbc29b4efe20199f98b0acb
rayne <wuxinlingwxl@163.com>
2019-03-05 09:44:54 +0800

    add_leetcode_63&64

---
+++ Array/medium/63. Unique Paths II.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    """
+    直接按照62的解题思路来做
+    Runtime: 24 ms, faster than 61.86% of Python online submissions for Unique Paths II.
+    Memory Usage: 10.9 MB, less than 22.36% of Python online submissions for Unique Paths II.
+    """
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        if not obstacleGrid or not obstacleGrid[0]:
+            return 0
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0]*n for _ in range(m)]
+        if obstacleGrid[0][0] == 0:
+            dp[0][0] = 1
+        else:
+            return 0
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    continue
+                if i > 0:
+                    dp[i][j] += dp[i-1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j-1]
+        return dp[-1][-1]
---
+++ Array/medium/64. Minimum Path Sum.py
@@ -0,0 +1,28 @@
+
+class Solution(object):
+    """
+    题目容易和62、63弄混了，该题的意思是在给定的双重数组里，找出从gird[0][0]到grid[-1][-1]的最短距离
+    所以原理还是和unique path相差不大，即最短距离就是当前的位置+左边/上边的最短路径，依次推导便是了
+    即遍历两次，每次取左边或者上边的最小值，但要考虑两种特殊情况：第一行和第一列，因为他们各自没有上边和左边，
+    因此当row=0，直接取左边的值为最小值，col=0.取右边的值为最小值
+    Runtime: 32 ms, faster than 94.90% of Python online submissions for Minimum Path Sum.
+    Memory Usage: 11.8 MB, less than 53.62% of Python online submissions for Minimum Path Sum.
+    """
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m,n = len(grid),len(grid[0])
+        for row in range(m):
+            for col in range(n):
+                if row == col == 0:
+                    before = 0
+                elif row == 0:
+                    before = grid[row][col-1]
+                elif col == 0:
+                    before = grid[row-1][col]
+                else:
+                    before = min(grid[row-1][col],grid[row][col-1])
+                grid[row][col] = before + grid[row][col]
+        return grid[-1][-1]
