796c8546c3cf0ac0deb122568efbceff39c175af
rayne <wuxinlingwxl@163.com>
2019-03-09 10:00:35 +0800

    add_leetcode_77&78

---
+++ Array/medium/79. Word Search.py
@@ -0,0 +1,35 @@
+
+
+class Solution(object):
+    """
+    循环查找的还是递归最实用，讨论区的大神答案，英文注释就已经写得非常清楚！
+    Runtime: 300 ms, faster than 32.05% of Python online submissions for Word Search.
+    Memory Usage: 14.1 MB, less than 39.51% of Python online submissions for Word Search.
+    """
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board,i,j,word):
+                    return True
+        return False
+    
+    # check whether can find word, start at (i,j) position    
+    def dfs(self, board, i, j, word):
+        if len(word) == 0: # all the characters are checked
+            return True
+        if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or word[0]!=board[i][j]:
+            return False
+        tmp = board[i][j]  # first character is found, check the remaining part
+        board[i][j] = "#"  # avoid visit agian 
+        # check whether can find "word" along one direction
+        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
+        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])
+        board[i][j] = tmp
+        return res
---
+++ Array/medium/80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,42 @@
+
+class MySolution(object):
+    """
+    思路简单，从第2个开始比，如果和前两个来的一致吗，则删除该数，否则继续
+    Runtime: 36 ms, faster than 86.94% of Python online submissions for Remove Duplicates from Sorted Array II.
+    Memory Usage: 10.5 MB, less than 99.09% of Python online submissions for Remove Duplicates from Sorted Array II.
+    """
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 2:
+            return
+        index = 2
+        while index and index <= len(nums)-1:
+            if nums[index] == nums[index-1] and nums[index] == nums[index-2]:
+                nums.pop(index)
+            else:
+                index += 1
+        return index
+        
+
+class Solution(object):
+    """
+    讨论区答案，一次遍历，当i>2的时候，如果遍历到的数增大则交换i的位置，否则继续遍历
+    Runtime: 32 ms, faster than 100.00% of Python online submissions for Remove Duplicates from Sorted Array II.
+    Memory Usage: 10.9 MB, less than 5.45% of Python online submissions for Remove Duplicates from Sorted Array II.
+    [1,1,1,2,2,3]
+    """
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        for n in nums:
+            if i < 2 or n > nums[i-2]:
+                nums[i] = n
+                i += 1
+        return i
+        
