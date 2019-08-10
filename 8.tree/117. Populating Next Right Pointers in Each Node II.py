4a38ff593f559e1b181f30f51efb563a146a0243
rayne <wuxinlingwxl@163.com>
2019-08-10 22:04:13 +0800

    add_leetcode_tree_117

---
+++ 8.tree/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,31 @@
+
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution:
+    """
+    就是把每层的节点放到一个队列里，把队列的每个元素进行弹出的时候，
+    如果它不是该层的最后一个元素，那么把它指向队列中的后面的元素（不把后面的这个弹出）
+    """
+    def connect(self, root: 'Node') -> 'Node':
+        if not root: return
+        queue = collections.deque()
+        queue.append(root)
+        while queue:
+            _len = len(queue)
+            for i in range(_len):
+                node = queue.popleft()
+                if i < _len - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
