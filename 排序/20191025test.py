8048ed0de074a5b4fba0213c3311cdd1f4a98ac3
rayne <wuxinlingwxl@163.com>
2019-10-28 16:20:35 +0800

    practice

--- 排序/20191025test.py
+++ 排序/20191025test.py
@@ -32,9 +32,11 @@ def choosesort(l):
 
 def insertsort(l):
     for i in range(1,len(l)):
-        for j in range(i,0,-1):
-            if l[j] < l[j-1]:
-                l[j],l[j-1] = l[j-1],l[j]
+        for j in range(i,-1,-1):
+            if l[j-1] > l[j]:
+                l.insert(j-1,l.pop(j))
+            else:
+                break
     return l
 
 l = [1,3,5,7,6,4,3,1,8,8,9,2,5,1,3,1,6,2]
