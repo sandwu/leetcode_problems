2d4155eff9d3a723b6f0a561c89479e42bdbce34
rayne <wuxinlingwxl@163.com>
2019-11-18 10:46:29 +0800

    tree_ordertravel

--- 专题训练/训练/树的遍历1117.py
+++ 专题训练/训练/树的遍历1117.py
@@ -30,10 +30,28 @@ class TreeTravel:
             res.append(node.val)
             stack.append(node.right)
             stack.append(node.left)
+        return res
 
     def in_order(self,root):
-        pass
-
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
 
     def post_order(self,root):
-        pass
+        res,stack = [],[]
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            res.append(node.val)
+            stack.append(node.left)
+            stack.append(node.right)
+        return res[::-1]
