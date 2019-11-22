5418f5102c303a0c708ae2f52e5a3e0cce7df7a8
rayne <wuxinlingwxl@163.com>
2019-11-22 09:40:14 +0800

    add_leetcode_test

--- 专题训练/训练/树的遍历1122.py
+++ 专题训练/训练/树的遍历1122.py
@@ -1,14 +1,9 @@
 
 
-class TreeNode:
-    def __init__(self,x):
-        self.val = x
-        self.left = None
-        self.right = None
 
 
 class TreeTravel:
-    def preorder(self,root):
+    def pre_order(self,root):
         res = []
         self.pre_helper(root,res)
         return res
@@ -19,7 +14,7 @@ class TreeTravel:
             self.pre_helper(root.left,res)
             self.pre_helper(root.right,res)
 
-    def inorder(self,root):
+    def in_order(self,root):
         res = []
         self.in_helper(root,res)
         return res
@@ -28,9 +23,9 @@ class TreeTravel:
         if root:
             self.in_helper(root.left,res)
             res.append(root.val)
-            self.in_helper(root.left,res)
+            self.in_helper(root.right,res)
 
-    def postorder(self,root):
+    def post_order(self,root):
         res = []
         self.post_helper(root,res)
         return res
@@ -41,41 +36,46 @@ class TreeTravel:
             self.post_helper(root.right,res)
             res.append(root.val)
 
-    def preorder_stack(self,root):
+
+
+class TreeTravel2:
+    def pre_order(self,root):
         res,stack = [],[]
         stack.append(root)
         while stack:
             node = stack.pop()
+            #当没有节点的时候，说明树已经遍历完了
             if not node:
-                continue
+                return res
+            #将根节点加入，再依次加入右、左节点，这样下一波循环先pop出来的是左节点
             res.append(node.val)
             stack.append(node.right)
             stack.append(node.left)
 
-    def inorder_stack(self,root):
-        res, stack = [], []
-        while True:
-            # 将root直接依次找到最左边的节点，都加到stack中
+
+    def in_order(self,root):
+        res,stack = [],[]
+        while stack:
+            #将root直接依次找到最左边的节点，都加到stack中
             while root:
                 stack.append(root)
                 root = root.left
-            if not stack:
-                return res
-            # 然后取出最左的节点加入结果集，在将该节点的右节点也加入stack，方便后续遍历
+            #然后取出最左的节点加入结果集，在将该节点的右节点也加入stack，方便后续遍历
             node = stack.pop()
+            if not node:
+                return res
             res.append(node.val)
             root = node.right
 
-
-    def postorder_stack(self,root):
-        if not root:return
+    def post_order(self,root):
+        #直接参考前序遍历，从根左右变成根右左，区别在于把res颠倒就行
         res,stack = [],[]
         stack.append(root)
         while stack:
-            node = stack.pop()
+            node =stack.pop()
+            if not node:
+                continue
             res.append(node.val)
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
+            stack.append(node.left)
+            stack.append(node.right)
         return res[::-1]
