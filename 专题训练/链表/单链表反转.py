d44595525754c2cdabd1e1c750ceccbe8268cfd9
rayne <wuxinlingwxl@163.com>
2019-11-25 11:26:01 +0800

    add_linked_list

---
+++ 专题训练/链表/单链表反转.py
@@ -0,0 +1,25 @@
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+    def reverseList2(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
