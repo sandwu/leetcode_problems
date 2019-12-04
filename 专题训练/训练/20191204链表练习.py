472313b29bdec36841625bf271579a2ac308ceed
rayne <wuxinlingwxl@163.com>
2019-12-04 12:11:04 +0800

    add_linklsit_practice

--- 专题训练/训练/20191204链表练习.py
+++ 专题训练/训练/20191204链表练习.py
@@ -19,3 +19,14 @@ class LinkedList:
     def removedupplication2(self,head):
         #有序链表，目的是删除所有的重复数，包括连唯一的那个也删除
         dummy = curr = ListNode(0)
+        curr.next = head
+        while head and head.next:
+            if head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                dummy.next = head
+            else:
+                head = head.next
+                dummy = dummy.next
+        return curr.next
