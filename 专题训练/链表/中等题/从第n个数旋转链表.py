d939c943849749bcecf74d1367c25858e5e28209
rayne <wuxinlingwxl@163.com>
2019-12-06 11:01:14 +0800

    add_linklsit_practice

--- 专题训练/链表/中等题/从第n个数旋转链表.py
+++ 专题训练/链表/中等题/从第n个数旋转链表.py
@@ -9,5 +9,14 @@ class Solution:
             pre,current = current,current.next
             n += 1 #可以记为一种求链表长度的方法
 
-        while not n or not k%n: #k%n表明如果k和总长度一致，则说明不用旋转，此时直接返回head即可
+        while not n or not k%n: #k%n表明如果k和总长度一致，则无须反转
             return head
+
+        dummy = head
+        for _ in range(n - k%n -1): #取要反转的前一个数
+            dummy = dummy.next
+
+        curr = dummy.next #这个数就是反转后的head
+        pre.next = head #原先指向null，将其指向当前的head
+        dummy.next = None #原有的dummy.next指向反转后的head，所以将当前的指向尾节点
+        return curr
