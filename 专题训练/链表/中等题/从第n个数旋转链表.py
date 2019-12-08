7b065d714aae26568dab64503f459f1ad0e7c4fd
rayne <wuxinlingwxl@163.com>
2019-12-08 23:35:25 +0800

    add_linklsit_practice

--- 专题训练/链表/中等题/从第n个数旋转链表.py
+++ 专题训练/链表/中等题/从第n个数旋转链表.py
@@ -19,5 +19,6 @@ class Solution:
         curr = dummy.next #这个数就是反转后的head
         pre.next = head #原先指向null，将其指向当前的head
         dummy.next = None #原有的dummy.next指向反转后的head，所以将当前的指向尾节点
+
         return curr
 
