f9678c199cb7e16289cca9b2a1e57731bd70eb53
rayne <wuxinlingwxl@163.com>
2020-03-03 23:14:09 +0800

    add_dp_dfs

--- 专题训练/数组/动态规划/322. Coin Change.py
+++ 专题训练/数组/动态规划/322. Coin Change.py
@@ -32,10 +32,48 @@ class Solution(object): #dfs超时
         for i in range(index ,len(coins)):
             self.dfs(i, coins, ans + 1, amount - coins[i])
 
+class Solution3(object): #dfs超时
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        self.res = float('inf')
+        self.tmp = {}
+        if amount == 0 :return 0
+        self.dfs(0 ,coins ,0 ,amount)
+        if self.res == float('inf') :return -1
+        return self.res
+
+    def dfs(self ,index ,coins ,ans ,amount):
+        if self.tmp.get(amount):return self.tmp[amount]
+        if amount < 0 :return 0
+        if amount == 0:
+            self.res = min(self.res ,ans)
+            self.tmp[amount] = self.res
+            return
+        for i in range(index ,len(coins)):
+            self.dfs(i, coins, ans + 1, amount - coins[i])
 
 
+class Solution2(object): #dp
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        MAX = float('inf')
+        dp = [0] + [MAX] * amount
 
-coins = [1, 2]
-amount = 2
-a = Solution()
+        for coin in coins:
+            for i in range(coin,amount+1):
+                if dp[i-coin] != MAX:
+                    dp[i] = min(dp[i],dp[i-coin] +1)
+        return dp[-1] if dp[-1] != MAX else -1
+
+coins = [1, 2,5]
+amount = 11
+a = Solution2()
 print(a.coinChange(coins, amount))
---
+++ 专题训练/数组/动态规划/39.combination sum.py
@@ -0,0 +1,43 @@
+
+
+
+
+#数组中和为目标值的所有集合1:允许重复利用一个值
+"""
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+"""
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        self.res = []
+        self.dfs(0,candidates,[],target)
+        return self.res
+
+    def dfs(self,index,candidates,path,target):
+        if target < 0:return
+        if target == 0:
+            self.res.append(path)
+            return
+        for i in range(index,len(candidates)):
+            self.dfs(i,candidates,path+[candidates[i]],target-candidates[i])
+
+
+class Solution2:
+
+    def combinationSum(self, candidates: [int], target: int) -> [[int]]:
+        answer = []
+
+        universe = [([], 0)]
+        for n in candidates:
+            for (ls, v) in universe:
+                if v + n == target:
+                    answer.append(ls + [n])
+                elif v + n < target:
+                    universe.append((ls + [n], v + n))
+
+        return answer
